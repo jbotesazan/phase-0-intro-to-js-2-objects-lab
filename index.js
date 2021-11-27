ee89f6bc504d638350ffa812a45cf831266f3859
Jason Botesazan <jbotesazan@gmail.com>
2021-11-27 18:46:50 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const employee = {
-    name: "Jason",
-    streetAdress: "7 Steven Lane"
+    name: 'Michael Jacksonovich',
+    streetAddress: '1 Wonderland',
 }
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = {...employee}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...employee};
 
-    newEmployee[key] = value
-    return newEmployee
+    newEmployee[key] = value;
+
+    return newEmployee;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value;
+
     return employee
 }
 
-function deleteFromEmployeeByKey(employee, key) {
-    const deletion = {...employee}
-    delete deletion[key]
-    return deletion
+function deleteFromEmployeeByKey(obj, key) {
+    const newEm = {...employee};
+
+    delete newEm[key];
+
+    return newEm
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    employee[key]
-    delete employee[key]
+destructivelyDeleteFromEmployeeByKey = (obj, key) => {
+    delete employee[key];
+
     return employee
 }
